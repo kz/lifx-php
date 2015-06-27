320066fc5c114e340c0f1b8c5677a650276c058c
Kelvin Zhang <me@iamkelv.in>
2015-06-27 12:28:15 +0100

    Create Lifx class

---
+++ src/Lifx.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kz\Lifx;
+
+class Lifx
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+}
